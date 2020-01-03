b36fa165f2210f4954187fd6d15d9b988a63feba
161905 <cristobal.carmona.14@sansano.usm.cl>
2020-01-03 19:03:36 -0300

    Controladores para modelo Water adicionales similares a los del modelo Food

--- app/Http/Controllers/Water/WaterController.php
+++ app/Http/Controllers/Water/WaterController.php
@@ -135,4 +135,170 @@ class WaterController extends ApiController
         $water->delete();
         return $this->showOne($water);
     }
+
+    public function indexcantidadA()
+    {
+        
+        /* 
+        SELECT t.id,t.cantidad,t.fechaFormato,t.hora FROM (SELECT *, DATE_FORMAT(fecha,'%d/%m/%Y') AS fechaFormato FROM water ORDER BY fecha,hora) t
+        */
+
+
+        $water = DB::table('water')
+            ->select('water.*',DB::raw("DATE_FORMAT(fecha, '%d/%m/%Y') as fechaFormato"))
+            ->orderBy('cantidad','asc')
+            ->orderBy('fecha','asc')
+            ->orderBy('hora', 'asc');
+
+        $waterFormato = DB::table( DB::raw("({$water->toSql()}) t") )
+            ->mergeBindings($water) 
+            ->select('t.id', 't.cantidad','t.fechaFormato','t.hora')
+            ->get();
+
+
+
+        //return response()->json($water,200);
+        return $this->showAll($waterFormato);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+
+    public function indexcantidadD()
+    {
+        
+        /* 
+        SELECT t.id,t.cantidad,t.fechaFormato,t.hora FROM (SELECT *, DATE_FORMAT(fecha,'%d/%m/%Y') AS fechaFormato FROM water ORDER BY fecha,hora) t
+        */
+
+
+        $water = DB::table('water')
+            ->select('water.*',DB::raw("DATE_FORMAT(fecha, '%d/%m/%Y') as fechaFormato"))
+            ->orderBy('cantidad','desc')
+            ->orderBy('fecha','desc')
+            ->orderBy('hora', 'desc');
+
+        $waterFormato = DB::table( DB::raw("({$water->toSql()}) t") )
+            ->mergeBindings($water) 
+            ->select('t.id', 't.cantidad','t.fechaFormato','t.hora')
+            ->get();
+
+
+
+        //return response()->json($water,200);
+        return $this->showAll($waterFormato);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+
+    public function indexfechaA()
+    {
+        
+        /* 
+        SELECT t.id,t.cantidad,t.fechaFormato,t.hora FROM (SELECT *, DATE_FORMAT(fecha,'%d/%m/%Y') AS fechaFormato FROM water ORDER BY fecha,hora) t
+        */
+
+
+        $water = DB::table('water')
+            ->select('water.*',DB::raw("DATE_FORMAT(fecha, '%d/%m/%Y') as fechaFormato"))
+            ->orderBy('fecha','asc')
+            ->orderBy('hora', 'asc');
+
+        $waterFormato = DB::table( DB::raw("({$water->toSql()}) t") )
+            ->mergeBindings($water) 
+            ->select('t.id', 't.cantidad','t.fechaFormato','t.hora')
+            ->get();
+
+
+
+        //return response()->json($water,200);
+        return $this->showAll($waterFormato);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+
+    public function indexfechaD()
+    {
+        
+        /* 
+        SELECT t.id,t.cantidad,t.fechaFormato,t.hora FROM (SELECT *, DATE_FORMAT(fecha,'%d/%m/%Y') AS fechaFormato FROM water ORDER BY fecha,hora) t
+        */
+
+
+        $water = DB::table('water')
+            ->select('water.*',DB::raw("DATE_FORMAT(fecha, '%d/%m/%Y') as fechaFormato"))
+            ->orderBy('fecha','desc')
+            ->orderBy('hora', 'desc');
+
+        $waterFormato = DB::table( DB::raw("({$water->toSql()}) t") )
+            ->mergeBindings($water) 
+            ->select('t.id', 't.cantidad','t.fechaFormato','t.hora')
+            ->get();
+
+
+
+        //return response()->json($water,200);
+        return $this->showAll($waterFormato);
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+
+    public function indexwater7()
+    {
+        
+        /* 
+        SELECT t.fechaFormato,SUM(t.cantidad) AS cantidadTotal,DAYNAME(t.fecha) AS dia FROM (SELECT *, DATE_FORMAT(fecha,'%d/%m/%Y') AS fechaFormato FROM water) t WHERE fecha > NOW() - INTERVAL 7 DAY GROUP BY fechaFormato ORDER BY fechaFormato;
+        */
+
+
+        $from = date('Y-m-d',strtotime("-6 days"));
+        $to = date('Y-m-d');
+
+        DB::statement("SET lc_time_names = 'es_ES'");
+
+
+        $water = DB::table('water')
+            ->select('water.*',DB::raw("DATE_FORMAT(fecha, '%d/%m/%Y') as fechaFormato"));
+
+
+        $waterFormato = DB::table( DB::raw("({$water->toSql()}) as t") )
+            ->mergeBindings($water) 
+            ->select('t.fechaFormato',DB::raw("DAYNAME(t.fecha) as dia,SUM(t.cantidad) as cantidadTotal"))
+            ->whereBetween('t.fecha',[$from,$to])
+            ->orderBy('t.fecha','asc')
+            ->groupBy('t.fecha')            
+            ->get();
+
+
+        foreach ($waterFormato as $ff) {
+            $ff->dia = ucwords($ff->dia);
+        }
+
+        //return response()->json($waterFormato,200, [],JSON_NUMERIC_CHECK);
+        return $this->showAll($waterFormato);
+    }
+
+
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -25,19 +25,19 @@ Route::get('foodFechaD', 'Food\FoodController@indexfechaD')->name('food.indexfec
 
 Route::get('food7', 'Food\FoodController@indexfood7')->name('food.indexfood7');
 
-Route::post('foodpublisher', 'Food\FoodController@foodpublisher')->name('food.foodpublisher');
+//Route::post('foodpublisher', 'Food\FoodController@foodpublisher')->name('food.foodpublisher');
 //Route::get('foodconsumer', 'Food\FoodController@foodconsumer')->name('food.foodconsumer');
 
 ####WATER CONTROLLERS####
 Route::resource('water','Water\WaterController');
 
-//Route::get('waterCantA', 'Water\WaterController@indexcantidadA')->name('water.indexcantidadA');
-//Route::get('waterCantD', 'Water\WaterController@indexcantidadD')->name('water.indexcantidadD');
+Route::get('waterCantA', 'Water\WaterController@indexcantidadA')->name('water.indexcantidadA');
+Route::get('waterCantD', 'Water\WaterController@indexcantidadD')->name('water.indexcantidadD');
 
-//Route::get('waterFechaA', 'Water\WaterController@indexfechaA')->name('water.indexfechaA');
-//Route::get('waterFechaD', 'Water\WaterController@indexfechaD')->name('water.indexfechaD');
+Route::get('waterFechaA', 'Water\WaterController@indexfechaA')->name('water.indexfechaA');
+Route::get('waterFechaD', 'Water\WaterController@indexfechaD')->name('water.indexfechaD');
 
-//Route::get('water7', 'Water\WaterController@indexfood7')->name('water.indexfood7');
+Route::get('water7', 'Water\WaterController@indexwater7')->name('water.indexwater7');
 
 
 ####ROUTINES CONTROLLERS####
