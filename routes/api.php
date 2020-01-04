ec6d0dbdaceca5d286a7b91cc00822e0bb44a9ae
161905 <cristobal.carmona.14@sansano.usm.cl>
2020-01-03 21:08:06 -0300

    Eliminacion de comentarios innecesarios

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,6 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-
 ####FOOD CONTROLLERS####
 Route::resource('food','Food\FoodController')->only([
     'index','store','show','update','destroy'
@@ -27,8 +15,6 @@ Route::get('foodFechaD', 'Food\FoodController@indexfechaD')->name('food.indexfec
 
 Route::get('food7', 'Food\FoodController@indexfood7')->name('food.indexfood7');
 
-//Route::post('foodpublisher', 'Food\FoodController@foodpublisher')->name('food.foodpublisher');
-//Route::get('foodconsumer', 'Food\FoodController@foodconsumer')->name('food.foodconsumer');
 
 ####WATER CONTROLLERS####
 Route::resource('water','Water\WaterController')->only([
@@ -56,9 +42,3 @@ Route::resource('waterRoutine', 'WaterRoutine\WaterRoutineController')->only([
 
 ###AUDIO CONTROLLERS####
 Route::resource('audio', 'Audio\AudioController');
-
-//Route::get('audio', 'Audio\AudioController@index')->name('audio.index');
-//Route::get('audio\{audio}', 'Audio\AudioController@show')->name('audio.show');
-//Route::post('audio', 'Audio\AudioController@store')->name('audio.store');
-//Route::post('audio\{audio}', 'Audio\AudioController@update')->name('audio.update');
-//Route::delete('audio\{audio}', 'Audio\AudioController@destroy')->name('audio.destroy');
